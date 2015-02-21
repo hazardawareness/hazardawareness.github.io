8a76170216179a1cfc5f5f917ac2f9ddd3f01790
Patrick Hazard <paddy@hazardawareness.com.au>
2015-02-21 10:07:07 +0800

    Push old site live
    Old hazardawareness site in prep for hosting portfolio on github

---
+++ js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+ 
+	$(".scroll").click(function(event){		
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+	});
+});
+
+
+	$(document).ready(function() {
+		$(".fancybox").fancybox();
+	});
